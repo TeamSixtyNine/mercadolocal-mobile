532093fa2a90912e61846240b12d8a5086070622
Danieru-San <fabio99alvarenga@gmail.com>
2020-07-03 19:31:29 -0300

    Remover client.js

--- src/client.js
+++
@@ -1,8 +0,0 @@
-var axios = require('react-native-axios');
-
-var axiosInstance = axios.create({
-	baseURL: 'http://192.168.25.12:5000',
-	/* other custom settings */
-});
-
-module.exports = axiosInstance;
