aa73d1130eb55703c7c0a4dee6a8ee24e5a40906
Danieru-San <fabio99alvarenga@gmail.com>
2020-07-05 09:27:15 -0300

    Adicionar tela de avaliação pós-checkout

--- src/pages/afterCheckout/index.js
+++ src/pages/afterCheckout/index.js
@@ -1,15 +1,81 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, AsyncStorage, TouchableHighlight } from 'react-native';
 import style from './style';
+import axios from 'axios';
+import { Rating, AirbnbRating } from 'react-native-elements';
+import { Feather } from '@expo/vector-icons';
 
-export default function afterCheckout({ route }) {
+export default function afterCheckout({ route, navigation }) {
 	const { productInfo } = route.params;
+	const [sellerNickname, setsellerNickname] = useState('');
+	(async function getSellerNickname() {
+		try {
+			const auth_token = await AsyncStorage.getItem('auth');
+			const ML_userInfo = `https://api.mercadolibre.com/users/${
+				productInfo.seller_id
+			}?access_token=${auth_token.split('"')[1]}`;
+			const response = await axios.get(ML_userInfo);
+			setsellerNickname(response.data.nickname);
+		} catch (err) {
+			console.log(err);
+		}
+	})();
+
+	function navigateToHomePage() {
+		navigation.navigate('paginaPrincipal');
+	}
+
+	// TODO: adicionar push notification para o usuário que receber a avaliação (pontuar, receber pedido de amigo, adicionar dinheiro ao caixa)
+	// TODO: pontuar também o usuário que comprou o produto
 
 	return (
 		<View style={style.container}>
-			<Text style={style.afterCheckoutTitle}>
-				Você acabou de comprar {productInfo.title}
-			</Text>
+			<Text style={style.afterCheckoutTitle}>Você acabou de comprar</Text>
+			<Text style={style.productAttribute}>{productInfo.title}</Text>
+			<Text style={style.productAttribute}>R$ {productInfo.price}</Text>
+			<Text style={style.productAttribute}>De {sellerNickname}</Text>
+
+			<View style={style.ratingContainer}>
+				<Text style={style.ratingText}>
+					Como você avaliaria a sua compra?
+				</Text>
+
+				<AirbnbRating
+					count={5}
+					reviews={['Péssimo', 'Ruim', 'Ok', 'Bom', 'Ótimo :)']}
+					defaultRating={5}
+					size={30}
+					reviewSize={20}
+					// onFinishRating={greet}
+					selectedColor={'#F4F1DE'}
+					reviewColor={'#F4F1DE'}
+					useNativeDriver={true}
+				/>
+			</View>
+
+			<View style={style.buttonContainer}>
+				<View style={style.friendRequestContainer}>
+					<Text style={style.friendRequest}>
+						Enviar solicitação de amizade para {sellerNickname}
+					</Text>
+					<Feather
+						style={style.frIcon}
+						name="user-plus"
+						size={24}
+						color="#fff"
+						// onPress={() => sendFriendRequest(paginaPrincipal)}
+					/>
+				</View>
+				<TouchableHighlight
+					style={style.confirmButton}
+					underlayColor="#7732a8"
+					onPress={() => navigateToHomePage()}
+				>
+					<Text style={style.confirmButtonText}>
+						Voltar para a Home
+					</Text>
+				</TouchableHighlight>
+			</View>
 		</View>
 	);
 }
--- src/pages/afterCheckout/style.js
+++ src/pages/afterCheckout/style.js
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import { Inter_500Medium } from '@expo-google-fonts/inter';
+import { normalize } from 'react-native-elements';
 
 export default StyleSheet.create({
 	container: {
@@ -14,12 +15,79 @@ export default StyleSheet.create({
 	afterCheckoutTitle: {
 		color: 'white',
 		fontFamily: 'Inter_500Medium',
+		fontSize: 25,
+	},
+
+	productAttribute: {
+		color: '#F4F1DE',
+		fontFamily: 'Inter_500Medium',
 		fontSize: 20,
+	},
+
+	ratingContainer: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 80,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	ratingText: {
+		marginTop: 60,
+		textAlign: 'center',
+		color: 'white',
+		fontFamily: 'Inter_500Medium',
+		fontSize: 20,
+		marginBottom: 30,
+	},
+
+	friendRequestContainer: {
+		alignItems: 'center',
+	},
+
+	friendRequest: {
+		marginTop: 50,
+		color: 'white',
+		fontFamily: 'Inter_500Medium',
+		fontSize: 15,
+		textAlign: 'center',
 	},
 
 	overlayText: {
 		color: 'black',
 		fontFamily: 'Inter_500Medium',
 		fontSize: 20,
+	},
+
+	frIcon: {
+		textAlign: 'center',
+		marginTop: 10,
+		marginBottom: 20,
+	},
+
+	confirmButtonText: {
+		color: 'white',
+		justifyContent: 'center',
+		alignSelf: 'center',
+		alignItems: 'center',
+	},
+
+	confirmButton: {
+		borderColor: 'white',
+		borderRadius: 20,
+		borderWidth: 1,
+		width: 200,
+		height: 50,
+		justifyContent: 'center',
+	},
+
+	buttonContainer: {
+		position: 'absolute',
+		bottom: 0,
+		alignSelf: 'center',
+		marginBottom: 20,
+		alignItems: 'center',
 	},
 });
